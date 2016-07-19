6e1b69d6d0cfc5d3847ccb5afda2dc9364a692a3
Mike Sausa <mikejsausa@gmail.com>
2016-07-18 20:41:34 -0600

    Commit to gh-pages

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ function generateRandomColor (){
   var r = generateRandomNumber(0, 255);
   var g = generateRandomNumber(0, 255);
   var b = generateRandomNumber(0, 255);
-if (r === 255 && g === 255 && b === 255){ generateRandomColor();}
-return "rgb(" + r + ", " + g + ", " + b +")";
+  if (r === 255 && g === 255 && b === 255){ generateRandomColor();}
+  else return "rgb(" + r + ", " + g + ", " + b +")";
 }
+
+$('.random').click(function(){
+  $('body').css("background-color", generateRandomColor());
+});
+
+
+$('.reset').click(function(){
+  $('body').css("background-color", "white");
+});
