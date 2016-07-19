1c14496ee61ffb6b1a6e463da46fbffb9eaa63da
sikemausa <sike.mausa@gmail.com>
2016-07-18 19:46:22 -0600

    Added functions for the random and reset buttons

--- script.js
+++ script.js
@@ -7,5 +7,14 @@ function generateRandomColor (){
   var g = generateRandomNumber(0, 255);
   var b = generateRandomNumber(0, 255);
 if (r === 255 && g === 255 && b === 255){ generateRandomColor();}
-return "rgb(" + r + ", " + g + ", " + b +")";
+else return "rgb(" + r + ", " + g + ", " + b +")";
 }
+
+$('.random').click(function(){
+  $('body').css("background-color", generateRandomColor());
+});
+
+
+$('.reset').click(function(){
+  $('body').css("background-color", "white");
+});
